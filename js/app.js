3ef2b84d8a7958c1ad5437f28d5e59819bc72801
Abdelrahman-Mohamed <abdelrahman.mohamed7337@gmail.com>
2024-10-19 12:05:27 +0300

    Add comments | Enhance styling

--- js/app.js
+++ js/app.js
@@ -5,33 +5,37 @@ const sections = document.querySelectorAll('section');
 //Add list items dynimaclly to the nav bar (ul)
 for (const section of sections) {
     const title = section.querySelector('h2').textContent;
-    const itemList = document.createElement('LI');
-    itemList.textContent = title;
-    itemList.className = section.id;
+    const listItem = document.createElement('LI');
+    listItem.textContent = title;
+    listItem.className = section.id;
     document.querySelector('ul').appendChild(itemList);
 }
 
 function makeActive(){
     for (let i = 0; i < sections.length; i++) {
+        //Get which section is being view
         const box = sections[i].getBoundingClientRect();
-        //Find a value that works best, but 150 seems to be a good start.
+        // Add active styling class to both the section and it's li in navbar
         if (box.top <= 500 && box.bottom >= 500) {
             sections[i].classList.add('active')
             document.querySelectorAll('li')[i].classList.add('active')
         } else {
             sections[i].classList.remove('active')
             document.querySelectorAll('li')[i].classList.remove('active')
-
         }
      }
 }
+// Listen to scroll events and call mackActive each time to update the viewd section with active styling class
 document.addEventListener('scroll', makeActive)
 
+//Listen to the click event on the unoreded list (Use event delgation)
 document.querySelector('ul').addEventListener('click', (event) => {
     const target = event.target;
     event.preventDefault();
     if(target.nodeName = "LI"){
+        // Get the corresponing section the li clicked
         const section = document.getElementById(target.classList[0]);
+        //Scroll smoothly to the desired section
         section.scrollIntoView({behavior: "smooth", block: "center"})
     }
 });
